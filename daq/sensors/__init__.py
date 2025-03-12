4e8bbe95227943db01aea08b963726cf00fdd087
Luis Leon <luis.leon@ridgerun.com>
2025-03-12 17:24:10 -0600

    Deconcentrate the sensor list

---
+++ daq/sensors/__init__.py
@@ -0,0 +1,21 @@
+from daq.sensors import example
+from daq import ISensor
+from enum import Enum
+from typing import Annotated
+
+class Sensors(Enum):
+    """
+    Enum to enumerate the supported sensors
+    """
+    EXAMPLE_SENSOR=Annotated[int, "Example sensor implementation"]
+
+def SensorBuilder(val: Sensors, name: str, logger: object) -> ISensor:
+    """
+    Sensor Builder Factory
+
+    This function is a factory function to generate the sensors and implement
+    them without accessing to the classes directly
+    """
+    if (val == Sensors.EXAMPLE_SENSOR):
+        return example.ExampleSensor(name, logger)
+    return None
